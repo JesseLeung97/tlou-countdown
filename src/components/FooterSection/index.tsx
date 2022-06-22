109c27c52b2a5ec875fff9bb658a9457e4114083
Jesse Leung <jessetleung@aol.com>
2022-06-22 23:55:15 +0900

    [Add][Update Upate colors

--- src/components/FooterSection/index.tsx
+++ src/components/FooterSection/index.tsx
@@ -9,9 +9,9 @@ const FooterSection = () => {
     return (
         <section className={classes.footer_wrapper}>
             <div className={classes.width_wrapper}>
-                <a href={texts.links.author} target="_blank" className={classes.footer_link}>{texts.footer.author}</a>
                 <span className={`${classes.open_about} ${classes.footer_link}`} onClick={() => setIsAboutOpen(true)}>{texts.about.aboutTitle}</span>
                 <a href={texts.links.githubLink} target="_blank" className={classes.footer_link}>{texts.footer.github}</a>
+                <a href={texts.links.author} target="_blank" className={classes.footer_link}>{texts.footer.author}</a>
                 <section className={`${classes.about_page} ${isAboutOpen ? classes.about_page_open : ""}`}>
                     <AboutOverlay onClose={setIsAboutOpen}/>
                 </section>
