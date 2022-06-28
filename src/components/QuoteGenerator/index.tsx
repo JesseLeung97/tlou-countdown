1f3648e31ed5cf803fb36a41255854e9fac95222
Jesse Leung <jessetleung@aol.com>
2022-06-28 11:42:52 +0900

    [Update] Refactor quote generator to use requestAnimationFrame

--- src/components/QuoteGenerator/index.tsx
+++ src/components/QuoteGenerator/index.tsx
@@ -9,16 +9,42 @@ const QuoteGenerator = () => {
 
     let updateInterval: ReturnType<typeof setInterval> | null = null;
     const [quote, setQuote] = useState<string>("");
+    const [isVisible, setIsVisible] = useState<boolean>(false);
     const { status } = useLoadingManager();
 
     useEffect(() => {
         if(status === "loading") return;
-        if(updateInterval === null) {
-            updateInterval = setInterval(() => {
-                pickQuote();
-            }, config.QUOTE_INTERVAL);
-        }
+        startUpdate();
     }, [status]);
+
+    const _duration = 9000;
+    const _start = 0;
+    const _keyframe_0 = (15 / 100) * _duration ;
+    const _keyFrame_1 = (25 / 100) * _duration;
+    const _keyframe_2 = (75 / 100) * _duration;
+    const _keyframe_3 = (85 / 100) * _duration;
+    const _end = 100;
+
+    const startUpdate = () => {
+        let startTime = new Date().getTime();
+        const update = () => {
+            const frameTime = new Date().getTime();
+            const dTime = frameTime - startTime;
+            if(dTime > _duration) {
+                startTime = new Date().getTime();
+                pickQuote();
+                requestAnimationFrame(update);
+                return;
+            }
+            if(dTime >= _keyframe_2) {
+                setIsVisible(false);
+            } else if(dTime >= _keyframe_0) {
+                setIsVisible(true);
+            } 
+            window.requestAnimationFrame(update);
+        }
+        update();
+    }
 
     const pickQuote = (): void => {
         const quotesLength = quotes.length;
@@ -28,7 +54,7 @@ const QuoteGenerator = () => {
 
     return (
         <div className={classes.quote_container}>
-            <h2 className={classes.shown_quote}>{quote !== "" ? `"${quote}"` : ""}</h2>
+            <h2 className={`${isVisible ? classes.quote_visible : ""} ${classes.shown_quote}`}>{quote !== "" ? `"${quote}"` : ""}</h2>
         </div>
     );
 }
